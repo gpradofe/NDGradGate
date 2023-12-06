abc70e09ceee302797021a37fc3cd4930262d57c
JonathanKarr33 <jka83321@gmail.com>
2023-12-06 03:17:00 -0500

    Reviewer Dashboard

--- FrontEnd/ndgradgate/src/Components/Pages/ReviewerDashboard.py/index.tsx
+++ FrontEnd/ndgradgate/src/Components/Pages/ReviewerDashboard.py/index.tsx
@@ -33,6 +33,7 @@ const ReviewerOverviewPage: React.FC = () => {
     value: fac.id,
   }));
 
+
   return (
     <DashboardContainer>
       <Toast ref={toast} />
@@ -41,21 +42,43 @@ const ReviewerOverviewPage: React.FC = () => {
           <Col md={12}>
             <Section>
               <Header>
-            <header className="header-container">
-              <div className="dashboard">
-                <h1>Reviewer Dashboard</h1>
-              </div>
-              <div className="user-info">
-                <p>Current User: Tim Weninger</p>
-              </div>
-          </header>
-          </Header>
-              <ReviewerDataGrid
-                data={assignedApplications}
-                onRecommendFaculties={recommendFaculties}
-                onRecommendApplicationDecision={recommendApplicationDecision}
-                facultyOptions={facultyOptions}
-              />
+                <header className="header-container">
+                  <div className="dashboard">
+                    <h1>Reviewer Dashboard</h1>
+                  </div>
+                  <div className="user-info">
+                    <p>Current User: Tim Weninger</p>
+                  </div>
+                </header>
+              </Header>
+              {/* Your existing table */}
+              <table style={{ width: "100%" }} >
+                <thead>
+                  <tr style={{ border: "1px solid black" }}>
+                    <th>First Name</th>
+                    <th>Applicant Status</th>
+                    <th>Area of Study</th>
+                    <th>Potential Faculty</th>
+                    <th>Comments</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr style={{ border: "1px solid black" }}>
+                    <td>Samuel</td>
+                    <td>In Progress</td>
+                    <td>Data Mining and Machine Learning</td>
+                    <td>
+                      <input type="checkbox" id="vehicle1" name="prof1" value="John Doe"></input>
+                      <label> John Doe</label><br></br>
+                      <input type="checkbox" id="prof2" name="prof2" value="Tim Weninger"></input>
+                      <label> Tim Weninger</label>
+                    </td>
+                    <td><form action="/submit" method="post">
+         <input type="text" id="comments_weninger" name="comments_weninger" placeholder="None"></input>
+    </form></td>
+                  </tr>
+                </tbody>
+              </table>
             </Section>
           </Col>
         </Row>
